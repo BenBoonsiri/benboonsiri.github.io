c0ba25fbfe15905b951ddda8421ce436b9f5cd77
Ben Boonsiri <benboonsiri@gmail.com>
2024-02-07 16:34:52 -0500

    jsx reformat

---
+++ ben-boonsiri-personal-site/src/Photos.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Photos() {
+    // const navigate = useNavigate();
+
+  return (
+    // <div style={{marginTop: '20px', marginRight: '30px', textAlign: 'right'}}>
+    // <Button variant="text" onClick={() => navigate('/about')} color="primary" style={{ marginRight: '20px', textTransform: 'none', fontSize: '18px', fontFamily: 'Nunito'}}>
+    //     About
+    // </Button>
+    // <Button variant="text" color="primary" onClick={() => navigate('/photos')} style={{ marginRight: '20px', textTransform: 'none' , fontSize: '18px', fontFamily: 'Nunito'}}>
+    //     Photos
+    // </Button>
+    // <Button variant="text" color="primary" onClick={() => navigate('/projects')} style={{ marginRight: '20px', textTransform: 'none', fontSize: '18px', fontFamily: 'Nunito' }}>
+    //     Projects
+    // </Button>
+    // </div>
+
+  );
+}
+
+export default Photos;
